ff91e3c403deda7d3238bcd15665eb95e6249b4a
Vladyslav Vasylenko <vladyslav.vasylenko@dev.pro>
2020-09-17 07:41:35 +0300

    init

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss'],
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O';
+}
